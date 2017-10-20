ac101cd06b0c67409cafd0e1ff490f9c4a42d94f
Dmitry S <dsagal+git@gmail.com>
2017-10-20 02:17:28 -0400

    Initial commit with a working and tested implementation

---
+++ README.md
@@ -0,0 +1,49 @@
+# collect-js-deps
+
+[![Build Status](https://travis-ci.org/gristlabs/yaml-schema-cfn.svg?branch=master)](https://travis-ci.org/gristlabs/yaml-schema-cfn)
+[![npm version](https://badge.fury.io/js/yaml-schema-cfn.svg)](https://badge.fury.io/js/yaml-schema-cfn)
+
+> Parser and schema for CloudFormation YAML template tags.
+
+Implements support for AWS-specific CloudFormation YAML schema.
+
+The implementation and tests are based on the official AWS Python client
+[aws-cli](https://github.com/aws/aws-cli). It supports all intrinsic CloudFormation functions listed in
+[AWS docs](http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/intrinsic-function-reference.html)
+as of October 2017.
+
+## Installation
+
+```
+npm install --save-dev yaml-schema-cfn
+```
+
+## Usage
+
+```
+const { yamlParse, yamlDump } = require('yaml-schema-cfn');
+
+const input = `
+Key:
+  - !GetAtt Foo.Bar
+  - !Equals [!Ref Baz, "hello"]
+`;
+
+const parsed = {
+  "Key": [
+    {"Fn::GetAtt", ["Foo": "Bar"]},
+    {"Fn::Equals", [{"Ref": "Baz", "hello"}]}
+  ]
+};
+
+assert.equal(yamlParse(input), parsed);
+assert.equal(yamlDump(parsed), input);
+```
+
+The module uses [js-yaml](https://github.com/nodeca/js-yaml). The schema it uses is also exported,
+and may be used e.g. like so:
+```
+const { schema } = require('yaml-schema-cfn');
+const yaml = require('js-yaml');
+yaml.safeLoad(input, { schema: schema })
+```
---
+++ test/test-main.js
@@ -0,0 +1,87 @@
+/**
+ * This test is based on the test in the official AWS python client:
+ * https://github.com/aws/aws-cli/blob/develop/tests/unit/customizations/cloudformation/test_yamlhelper.py
+ */
+"use strict";
+
+/* global describe, it */
+
+const assert = require('assert');
+const { yamlParse, yamlDump } = require('../index');
+
+
+describe('yaml-schema', function() {
+  const yamlWithTags = `
+  Resource:
+      Key1: !Ref Something
+      Key2: !GetAtt Another.Arn
+      Key3: !Equals [!Base64 YetAnother, "hello"]
+      Key4: !Sub {"a": "1"}
+      Key5: !GetAtt OneMore.Outputs.Arn
+      Key6: !Condition OtherCondition
+  `;
+
+  const parsedYamlDict = {
+    "Resource": {
+      "Key1": {
+        "Ref": "Something"
+      },
+      "Key2": {
+        "Fn::GetAtt": ["Another", "Arn"]
+      },
+      "Key3": {
+        "Fn::Equals": [
+          {"Fn::Base64": "YetAnother"},
+          "hello"
+        ]
+      },
+      "Key4": {
+        "Fn::Sub": {
+          "a": "1"
+        }
+      },
+      "Key5": {
+        "Fn::GetAtt": ["OneMore", "Outputs.Arn"]
+      },
+      "Key6": {
+        "Condition": "OtherCondition"
+      }
+    }
+  };
+
+  it("should parse yaml with tags", function() {
+    const output = yamlParse(yamlWithTags);
+    assert.deepEqual(output, parsedYamlDict);
+
+    // Make sure formatter and parser work well with each other.
+    const formatted = yamlDump(output);
+    const outputAgain = yamlParse(formatted);
+    assert.deepEqual(output, outputAgain);
+  });
+
+  it("should handle GetAtt even if invalid", function() {
+    // This tests is as it exists in the python test at https://github.com/aws/aws-cli.
+    // This is an invalid syntax for !GetAtt. But make sure the code does not crash when we encouter this syntax.
+    // Let CloudFormation interpret this value at runtime.
+    const input = `
+    Resource:
+      Key: !GetAtt ["a", "b"]
+    `;
+    const output = {
+      "Resource": {
+        "Key": {
+          "Fn::GetAtt": ["a", "b"]
+        }
+      }
+    };
+
+    const actualOutput = yamlParse(input);
+    assert.deepEqual(actualOutput, output);
+  });
+
+  it("should handle json with tabs", function() {
+    const template = '{\n\t"foo": "bar"\n}';
+    const output = yamlParse(template);
+    assert.deepEqual(output, {'foo': 'bar'});
+  });
+});
