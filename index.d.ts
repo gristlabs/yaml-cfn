b0a93ed606a98aca6b5900f806386fd0ff7fa01f
JadenSimon <31319484+JadenSimon@users.noreply.github.com>
2021-06-30 12:33:20 -0700

    Fix wrong type
    Exported object is a Schema not a SchemaDefinition

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 // Type definitions for yaml-cfn.
 
-import {SchemaDefinition} from "js-yaml";
+import {Schema} from "js-yaml";
 
-export const schema: SchemaDefinition;
+export const schema: Schema;
 
 export function yamlParse(str: string): any;
 export function yamlDump(obj: any): string;
